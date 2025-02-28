bcdf5baf5b2f0568b231d523fe04931b17684b84
Energisse <thomas.halvick@gmail.com>
2025-02-28 19:23:56 +0100

    ajout feedback

--- webapp/api.py
+++ webapp/api.py
@@ -2,7 +2,6 @@ from io import StringIO
 import numpy as np
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid,GridOptionsBuilder, JsCode
 import base64
 import requests
 
@@ -10,52 +9,79 @@ def image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode("utf-8")
 
+# Convertir les images en Base64
 deathImage = f"data:image/svg+xml;base64,{image_to_base64('./images/death-skull-and-bones-svgrepo-com.svg')}"
 aliveImage = f"data:image/svg+xml;base64,{image_to_base64('./images/heart-svgrepo-com.svg')}"
 
-renderImage = JsCode("""
-class ThumbnailRenderer {
-    init(params) {
-
-    this.eGui = document.createElement('img');
-    this.eGui.setAttribute('src', params.value);
-    this.eGui.setAttribute('width', 'auto');
-    this.eGui.setAttribute('height', '100%');
-    }
-        getGui() {
-        return this.eGui;
-    }
-}
- """)    
-
 st.set_page_config(layout="wide")
-st.write("""# Vas-tu mourir ?""")
+st.title("Vas-tu mourir ?")
+
+if 'pd_data' not in st.session_state:
+    st.session_state.pd_data = None
 
 with st.form(key='file_upload_form', clear_on_submit=True):
     uploaded_file = st.file_uploader("Upload a file", type=["csv"])
     submit_button = st.form_submit_button(label='Prédiction')
-    
-    
-if submit_button:
 
+if submit_button and uploaded_file is not None:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
     pd_data = pd.read_csv(StringIO(bytes_data.decode('utf-8')))
     
     json_data = pd_data.replace({np.nan: None}).to_dict(orient='records')
-    
-    response = requests.post("http://localhost:8080/predict", json=json_data )
+    response = requests.post("http://localhost:8080/predict", json=json_data)
     res = response.json()
     
-    # Add a column to the dataframe
+    # Ajout des prédictions sous forme d'images
     predictions = res.get('predictions', [])
+    pd_data["prediction"] = predictions
+    pd_data["target"] = [None] * len(pd_data)
 
-    pd_data["result"] = [deathImage if x == 1 else aliveImage for x in predictions]
-    
-    gridOption = GridOptionsBuilder.from_dataframe(pd_data)
-    gridOption.configure_column("result", cellRenderer=renderImage,headerName="Status",width=100,pinned="right")
-    gridOptions = gridOption.build()
-    AgGrid(pd_data, gridOptions=gridOptions, allow_unsafe_jscode=True)
+    st.session_state.pd_data = pd_data
 
-    # Clear the file uploader
-    uploaded_file = None
+if st.session_state.pd_data is not None:
+    pd_data = st.session_state.pd_data
+    st.write("### Résultats des prédictions")
+
+    # Affichage ligne par ligne sous forme de tableau
+    for index, row in pd_data.iterrows():
+        st.write(f"#### Ligne {index + 1}")
+
+        # Transformation de la ligne en dataframe pour affichage dans `st.table`
+        row_df = pd.DataFrame(row).transpose()
+        row_df = row_df.drop(columns=["prediction","target"])  # Supprimer les colonnes image et prediction pour éviter un bug d'affichage
+
+        # Affichage du tableau
+        st.table(row_df)
+
+        st.write(f"##### Prediction")
+
+        # Affichage de l'image du résultat et du label de prédiction
+        col1, col2, col3 = st.columns([1, 1, 8])
+        with col1:
+            st.image( deathImage if row["prediction"] == 1 else aliveImage, width=50)
+        
+        st.write(f"##### Feedback")
+
+        if st.session_state.pd_data.at[index, "target"] is not None:
+            st.image(deathImage if st.session_state.pd_data.at[index, "target"] == 1 else aliveImage, width=50)
+        else:
+        # Boutons de feedback avec images
+            col4, col5 = st.columns([1, 1])
+            with col4:
+                if st.button("💀", key=f"death_{index}"):
+                    st.session_state.pd_data.at[index, "target"] = 1
+                    data = pd_data.iloc[index].replace({np.nan: None}).to_dict()
+                    print(data)
+                    requests.post("http://localhost:8080/feedback", json=[data])
+                    st.rerun()
+
+            with col5:
+                if st.button("❤️", key=f"alive_{index}"):
+                    st.session_state.pd_data.at[index, "target"] = 0
+                    data = pd_data.iloc[index].replace({np.nan: None}).to_dict()
+                    print(data)
+                    
+                    requests.post("http://localhost:8080/feedback", json=[data])
+                    st.rerun()
+
+        st.write("---")  # Séparateur entre chaque ligne
