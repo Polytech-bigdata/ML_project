926c9abf3422d29ca38b13566121e6c09a815571
ThomasBlumet <thomas.blumet@orange.fr>
2025-03-03 19:43:45 +0100

    update project file in reporting

--- reporting/project.py
+++ reporting/project.py
@@ -3,36 +3,27 @@ import datetime
 import pandas as pd
 
 from evidently import ColumnMapping
-from evidently.future.datasets import DataDefinition, Dataset
-from evidently.metrics import ColumnDriftMetric
-from evidently.metrics import ColumnSummaryMetric
-from evidently.metrics import DatasetDriftMetric
-from evidently.metrics import DatasetMissingValuesMetric
-from evidently.metrics import ClassificationConfusionMatrix
+from evidently.metrics import ColumnDriftMetric, ColumnSummaryMetric,DatasetDriftMetric,DatasetMissingValuesMetric
 from evidently.report import Report
 from evidently.test_preset import DataDriftTestPreset
 from evidently.test_suite import TestSuite
-from evidently.ui.dashboards import CounterAgg
-from evidently.ui.dashboards import DashboardPanelCounter
-from evidently.ui.dashboards import DashboardPanelPlot
-from evidently.ui.dashboards import PanelValue
-from evidently.ui.dashboards import PlotType
-from evidently.ui.dashboards import ReportFilter
-from evidently.ui.remote import RemoteWorkspace
-from evidently.ui.workspace import Workspace
-from evidently.ui.workspace import WorkspaceBase
+from evidently.ui.dashboards import CounterAgg,DashboardPanelCounter,DashboardPanelPlot,PanelValue,PlotType,ReportFilter
+from evidently.ui.workspace import Workspace,WorkspaceBase
 from evidently.metric_preset import ClassificationPreset
-from prometheus_client import Gauge, start_http_server
-import time
+from evidently.metrics.custom_metric import CustomValueMetric
+from evidently.base_metric import InputData
+from evidently.renderers.html_widgets import WidgetSize
 
-from scripts.utils import load_heterogeneous_dataset, load_pipeline
+from prometheus_client import Gauge, start_http_server
+from sklearn.metrics import balanced_accuracy_score
+from scripts.utils import load_pipeline
 
 REF_DATA_FILEPATH = 'data/ref_data.csv'
 PROD_DATA_FILEPATH = 'data/prod_data.csv'
 
 WORKSPACE = "workspace"
-YOUR_PROJECT_NAME = "ICU Mortality Prediction"
-YOUR_PROJECT_DESCRIPTION = "BigData project using a kaggle dataset for hospital mortality prediction."
+PROJECT_NAME = "ICU Mortality Prediction"
+PROJECT_DESCRIPTION = "BigData project using a kaggle dataset for hospital mortality prediction."
 
 #for analizing the drift about the most interesting features in the dataset
 NB_INTERESTING_FEATURES = 5
@@ -41,19 +32,7 @@ ref_data = pd.read_csv(REF_DATA_FILEPATH, delimiter=";")
 model = load_pipeline("artifacts/model.pkl")
 ref_data["prediction"] = model.predict(ref_data.iloc[:, :-1])
 prod_data = pd.read_csv(PROD_DATA_FILEPATH, delimiter=";")
-# ref_data , ref_y, ref_col_num, ref_col_cat, ref_labels = load_heterogeneous_dataset(REF_DATA_FILEPATH, delimiter=";", predict_var=False)
-# prod_data, prod_y, prod_col_num, prod_col_cat, prod_labels = load_heterogeneous_dataset(PROD_DATA_FILEPATH, delimiter=";", predict_var=False)
-# prod_data = prod_data.iloc[:, :-1]
 features = pd.read_csv("data/features_sorted_by_importance.csv")
-
-
-#define metrics used in the report
-clf_metrics = {
-    'F1 score' : "f1",
-    'Balanced Accuracy' : "balanced_accuracy",
-    'Precision' : "precision",
-    'Recall' : "recall",
-}
 
 #define columns on which drift are interesting to monitor
 features_list = features.iloc[:, 0].tolist()
@@ -67,18 +46,28 @@ column_mapping = ColumnMapping(
     prediction='prediction', 
 )
 
+#create a metric function for balanced_accuracy (both current and ref data) to display the value in the dashboard
+def current_balanced_accuracy_func(data: InputData): 
+    return balanced_accuracy_score(data.current_data[data.column_mapping.target],
+        data.current_data[data.column_mapping.prediction])
+
+def ref_balanced_accuracy_func(data: InputData): 
+    return balanced_accuracy_score(data.reference_data[data.column_mapping.target],
+        data.reference_data[data.column_mapping.prediction])
+    
+
 def create_report():
     data_drift_report = Report(
         metrics=[
             DatasetDriftMetric(),
             DatasetMissingValuesMetric(),
-            ClassificationConfusionMatrix(),
+            ClassificationPreset(),#contains all the classification metrics (Accuracy, F1 score, precision, recall) and display the confusion matrix
+            CustomValueMetric(func=current_balanced_accuracy_func, title="Current : Balanced Accuracy", size=WidgetSize.HALF),
+            CustomValueMetric(func=ref_balanced_accuracy_func, title="Reference : Balanced Accuracy", size=WidgetSize.HALF),
             *drift_columns,
-            ClassificationPreset(),
         ],
         timestamp=datetime.datetime.now(),
     )
-
     data_drift_report.run(reference_data=ref_data, current_data=prod_data, column_mapping=column_mapping)
     return data_drift_report
 
@@ -94,27 +83,24 @@ def create_test_suite():
 
 
 def create_project(workspace: WorkspaceBase):
-    project = workspace.create_project(YOUR_PROJECT_NAME)
-    project.description = YOUR_PROJECT_DESCRIPTION
-
-    #add metrics panels to the dashboard
-    for metric_name in clf_metrics.keys():
-        project.dashboard.add_panel(
-            DashboardPanelCounter(
-                title=metric_name,
-                filter=ReportFilter(metadata_values={}, tag_values=[]),
-                value=PanelValue(
-                    metric_id="ClassificationQualityMetric",
-                    metric_args= {"metric": clf_metrics[metric_name]},
-                    field_path="value",               
-                    legend=metric_name,
-                ),
-                text=metric_name,
-                agg=CounterAgg.LAST,
-                size=1,
-            )
-        )
-        
+    project = workspace.create_project(PROJECT_NAME)
+    project.description = PROJECT_DESCRIPTION
+    # project.dashboard.add_panel(
+    #     DashboardPanelCounter(
+    #         title="Balanced Accuracy",
+    #         filter=ReportFilter(metadata_values={}, tag_values=[]),
+    #         value=PanelValue(
+    #             metric_id="CustomValueMetric",
+    #             metric_args={"title": "Balanced Accuracy"},
+    #             field_path=CustomValueMetric.fields.value,
+    #             legend="count",
+    #         ),
+    #         text="count",
+    #         agg=CounterAgg.LAST,
+    #         size=WidgetSize.HALF,
+    #     )
+    # )
+    
     project.dashboard.add_panel(
         DashboardPanelCounter(
             title="Share of Drifted Features",
@@ -129,6 +115,7 @@ def create_project(workspace: WorkspaceBase):
             size=1,
         )
     )
+
     project.dashboard.add_panel(
         DashboardPanelPlot(
             title="Dataset Quality",
@@ -140,43 +127,31 @@ def create_project(workspace: WorkspaceBase):
                     field_path=DatasetMissingValuesMetric.fields.current.share_of_missing_values,
                     legend="Missing Values Share",
                 ),
-                PanelValue(
-                    metric_id="ClassificationQualityMetric",
-                    metric_args={"metric": "f1"},
-                    field_path="value",
-                    legend="F1 Score",
-                ),
-                PanelValue(
-                    metric_id="ClassificationQualityMetric",
-                    metric_args={"metric": "balanced_accuracy"},
-                    field_path="value",
-                    legend="Balanced Accuracy",
-                ),
             ],
             plot_type=PlotType.LINE,
         )
     )
 
+    features_values = []
     for i in range(0, NB_INTERESTING_FEATURES):
-        title = str(features_list[i]) + " : Wasserstein drift distance"
-        column_name = features_list[i]
-        project.dashboard.add_panel(
-            DashboardPanelPlot(
-                title=title,
-                filter=ReportFilter(metadata_values={}, tag_values=[]),
-                values=[
-                    PanelValue(
-                        metric_id="ColumnDriftMetric",
-                        metric_args={"column_name.name": column_name},
-                        field_path=ColumnDriftMetric.fields.drift_score,
-                        legend="Drift Score",
-                    ),
-                ],
-                plot_type=PlotType.BAR,
-                size=1,
+        features_values.append(
+            PanelValue(
+                metric_id="ColumnDriftMetric",
+                metric_args={"column_name.name": features_list[i]},
+                field_path=ColumnDriftMetric.fields.drift_score,
+                legend=features_list[i],
             )
         )
-    
+    project.dashboard.add_panel(
+        DashboardPanelPlot(
+            title=f"Drift for the {NB_INTERESTING_FEATURES} most important dataset features (Wasserstein drift distance)",
+            filter=ReportFilter(metadata_values={}, tag_values=[]),
+            values=features_values,
+            plot_type=PlotType.BAR,
+            size=1,
+        )
+    )
+
     project.save()
     return project
 
